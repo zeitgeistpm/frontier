aa640bf4acbc4cfb632d7c30e86bfc4ae8d81097
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2023-08-18 08:50:17 +0200

    compute the storage quota per tx

--- frame/evm/precompile/dispatch/src/mock.rs
+++ frame/evm/precompile/dispatch/src/mock.rs
@@ -153,6 +153,7 @@ impl pallet_evm::Config for Test {
 	type OnCreate = ();
 	type FindAuthor = FindAuthorTruncated;
 	type GasLimitPovSizeRatio = ();
+	type GasLimitStorageGrowthRatio = ();
 	type Timestamp = Timestamp;
 	type WeightInfo = ();
 }
